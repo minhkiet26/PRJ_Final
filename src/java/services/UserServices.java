48082a517175383f5de26eac424213da79a93e35
duongbilly18012004@gmail.com <duongbilly18012004@gmail.com>
2026-01-31 14:53:39 +0700

    WaitUserInforDetail

--- src/java/services/UserServices.java
+++ src/java/services/UserServices.java
@@ -15,15 +15,18 @@ import sun.security.util.Password;
 
 public class UserServices {
 
-    public String getUser(String email, String password) {
+    public User getUser(String email, String password) {
         Connection cn = null;
-        String role = null;
+        User u = null;
         try {
             cn = DBUtils.getConnection();
             if (cn != null) {
-                String sql = "select [Role]\n"
-                        + "from [dbo].[User]\n"
-                        + "where Email = ? and Password = ?";
+                String sql = "SELECT [Email]\n"
+                        + "      ,[Password]\n"
+                        + "      ,[Role]\n"
+                        + "      ,[PhoneNumber]\n"
+                        + "  FROM [OnlineCourseDB].[dbo].[User]\n"
+                        + "  WHERE Email = ? and Password = ?";
                 //chuẩn bị cho câu sql gửi đi
                 PreparedStatement st = cn.prepareStatement(sql);
                 st.setString(1, email);
@@ -32,15 +35,18 @@ public class UserServices {
                 ResultSet table = st.executeQuery();
                 if (table != null) {
                     while (table.next()) {
+                        String Email = table.getString("Email");
+                        String Password = table.getString("Password");
                         String Role = table.getString("Role");
-                        role = Role;
+                        String PhoneNumber = table.getString("PhoneNumber");
+                        u = new User(Email, Password, PhoneNumber, Role);
                     }
                 }
             }
         } catch (Exception e) {
 
         }
-        return role;
+        return u;
     }
 
     public Student getStudent(String email) {
